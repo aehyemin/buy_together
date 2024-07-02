ccf92eb5517a20ad8c6730ba1f502f399e2dad39
Minkyoung00 <kina12345600@gmail.com>
2024-07-02 20:56:15 +0900

    복구

--- app.py
+++ app.py
@@ -14,41 +14,50 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/memo', methods=['POST'])
+@app.route('/product', methods=['POST'])
 def post_article():
     # 1. 클라이언트로부터 데이터를 받기
     url_receive = request.form['url_give']  # 클라이언트로부터 url을 받는 부분
+    end_receive = request.form['end_give']
+    min_receive = request.form['min_give']
+    max_receive = request.form['max_give']
+    account_receive = request.form['account_give']
     comment_receive = request.form['comment_give']  # 클라이언트로부터 comment를 받는 부분
 
     # 2. meta tag를 스크래핑하기
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
-    soup = BeautifulSoup(data.text, 'html.parser')
+    # headers = {
+    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    # data = requests.get(url_receive, headers=headers)
+    # soup = BeautifulSoup(data.text, 'html.parser')
 
-    og_image = soup.select_one('meta[property="og:image"]')
-    og_title = soup.select_one('meta[property="og:title"]')
-    og_description = soup.select_one('meta[property="og:description"]')
+    # og_image = soup.select_one('meta[property="og:image"]')
+    # og_title = soup.select_one('meta[property="og:title"]')
+    # og_description = soup.select_one('meta[property="og:description"]')
 
-    url_title = og_title['content']
-    url_description = og_description['content']
-    url_image = og_image['content']
+    # url_title = og_title['content']
+    # url_description = og_description['content']
+    # url_image = og_image['content']
+    
+    # ------
+    user_list = []
 
-    article = {'url': url_receive, 'title': url_title, 'desc': url_description, 'image': url_image,
-               'comment': comment_receive}
+    products = {'url': url_receive, 
+            #    'name': url_title, 'image': url_description, 'price': url_image,
+               'end': end_receive, 'min': min_receive, 'max': max_receive, 'account': account_receive, 'comment': comment_receive,
+               'join': user_list }
 
     # 3. mongoDB에 데이터를 넣기
-    db.articles.insert_one(article)
+    db.products.insert_one(products)
 
     return jsonify({'result': 'success'})
 
 
-@app.route('/memo', methods=['GET'])
+@app.route('/product', methods=['GET'])
 def read_articles():
     # 1. mongoDB에서 _id 값을 제외한 모든 데이터 조회해오기 (Read)
-    result = list(db.articles.find({}, {'_id': 0}))
+    result = list(db.products.find({}, {'_id': 0}))
     # 2. articles라는 키 값으로 article 정보 보내주기
-    return jsonify({'result': 'success', 'articles': result})
+    return jsonify({'result': 'success', 'products': result})
 
 
 if __name__ == '__main__':
