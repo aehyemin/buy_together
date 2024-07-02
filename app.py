4dcd57adc1b109077cdeb986722c76fce34be9b2
rabit9999 <biomatrix117@naver.com>
2024-07-02 09:25:46 +0900

    gpt 코드 다듬고 기본 리디렉션 구현

--- app.py
+++ app.py
@@ -15,10 +15,6 @@ app.config['SECRET_KEY'] = '아무거나'
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.dbjungle  # 'dbjungle'라는 이름의 db를 만들거나 사용합니다.
 
-# @app.route('/')
-# def home():
-#     return render_template('home.html')
-
 
 @app.route('/memo', methods=['POST'])
 def post_article():
@@ -57,80 +53,85 @@ def read_articles():
     return jsonify({'result': 'success', 'articles': result})
 
 
-##### 로그인 구현 #####
+##### 로그인, 회원가입 구현 #####
 
-# 세션 데이터를 암호화하기 위한 비밀 키 설정
-app.secret_key = 'secretkey'
-
-# 가상의 사용자 데이터베이스
-users = db.users
+app.secret_key = 'secretkey' # 비밀 키
+users = db.users # 유저 DB
 
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = request.cookies.get('token')
         if not token:
-            flash('Token is missing!')
-            return redirect(url_for('login'))
+            flash('토큰이 없습니다')
+            return redirect(url_for('login_get'))
         try:
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
             current_user = users.find_one({'username': data['username']})
         except:
-            flash('Token is invalid!')
-            return redirect(url_for('login'))
+            flash('틀린 토큰입니다')
+            return redirect(url_for('login_get'))
         return f(current_user, *args, **kwargs)
     return decorated
 
+# 홈
 @app.route('/')
-def home():
-    return render_template('home.html')
+@token_required # 토큰이 있는지 (로그인 돼있는지 확인)
+def start():
+    return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = users.find_one({'username': username})
-        
-        if user and check_password_hash(user['password'], password):
-            token = jwt.encode({
-                'username': username,
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
-            }, app.config['SECRET_KEY'], algorithm="HS256")
-            response = make_response(redirect(url_for('index')))
-            response.set_cookie('token', token)
-            return response
-        else:
-            flash('Invalid username or password')
-    
+# @app.route('/index')
+# @token_required
+# def index(current_user):
+#     return render_template('index.html', username=current_user['username'])
+
+# 로그인
+@app.route('/login', methods=['GET'])
+def login_get():
     return render_template('login.html')
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        
-        if users.find_one({'username': username}):
-            flash('Username already exists')
-        else:
-            hashed_password = generate_password_hash(password, method='sha256')
-            users.insert_one({'username': username, 'password': hashed_password})
-            flash('User registered successfully')
-            return redirect(url_for('login'))
+@app.route('/login', methods=['POST'])
+def login_post():
+    username = request.form['username']
+    password = request.form['password']
+    user = users.find_one({'username': username})
     
+    if user and check_password_hash(user['password'], password):
+        token = jwt.encode({
+            'username': username,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+        }, app.config['SECRET_KEY'], algorithm="HS256")
+        response = make_response(redirect(url_for('index')))
+        response.set_cookie('token', token)
+        return response
+    else:
+        flash('Invalid username or password')
+
+# 회원가입
+@app.route('/register', methods=['GET'])
+def register_get():
     return render_template('register.html')
 
+@app.route('/register', methods=['POST'])
+def register_post():
+    username = request.form['username']
+    password = request.form['password']
+    
+    if users.find_one({'username': username}):
+        flash('Username already exists')
+    else:
+        hashed_password = generate_password_hash(password, method='sha256')
+        users.insert_one({'username': username, 'password': hashed_password})
+        flash('User registered successfully')
+        return redirect(url_for('login'))
+    
+# 로그아웃
 @app.route('/logout')
 def logout():
     response = make_response(redirect(url_for('login')))
     response.delete_cookie('token')
     return response
 
-@app.route('/index')
-@token_required
-def index(current_user):
-    return render_template('index.html', username=current_user['username'])
-
+# 앱 실행
 if __name__ == '__main__':
     app.run(debug=True)
